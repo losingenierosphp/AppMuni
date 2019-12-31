dc1cafb8a7af838157394a3cfac9b52603c374b0
unknown <said@ayudantee.com>
2019-12-30 23:19:28 -0300

    edito el readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # AppMuni
+
+### inicializar el proyecto
+
+primero clonar el proyecto 
+
+a continuación ingresar a la carpeta AppMuni
+
+luego instalar las dependencias 
+
+
+
+```
+git clone https://github.com/losingenierosphp/AppMuni.git
+
+cd AppMuni
+
+npm install
+```
+
--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const { app, BrowserWindow } = require('electron')
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+   host:'35.226.224.5',
+   user: 'root',
+   password:'root',
+   database: 'dataMuni'
+})
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS result', function (error, results, fields) {
+  if (error) throw error;
+  console.log('1 + 1 = ', results[0].result); 
+});
+
+connection.end();  
 
 function createWindow () {
   // Create the browser window.
