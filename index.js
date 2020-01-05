5b2efab658fc577dec878be42226c92b96ca2cc1
unknown <said@ayudantee.com>
2020-01-05 19:19:42 -0300

    logos y menu editado

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require('electron')
-const mysql = require('mysql')
+const { app, BrowserWindow,Menu } = require('electron')
+const shell = require('electron').shell
+//const mysql = require('mysql')
 /*
 const connection = mysql.createConnection({
    host:'35.226.224.5',
@@ -18,20 +19,42 @@ connection.query('SELECT 1 + 1 AS result', function (error, results, fields) {
 connection.end();  
 */
 function createWindow () {
-  // Create the browser window.
+  // crea una ventana 
   let win = new BrowserWindow({
     width: 800, //ancho de la ventana
     height: 600, //altura de la entana 
-    frame: false, //ventana sin bordes 
-   
+   // frame: false, //ventana sin bordes 
+    icon: "vistas/img/icon.png",
     webPreferences: {
       nodeIntegration: true,
       devTools: false //bloquear el developer tools de los navegadores 
     }
-  })
+  }
+  )
 
   // and load the index.html of the app.
   win.loadFile('vistas/index.html')
+
+  //menu personalizado de la ventana electron 
+      var menu = Menu.buildFromTemplate([
+        {
+            label: 'INICIO',
+            submenu: [
+                {label:'ir al la documentacion del proyecto',
+                click() { 
+                  shell.openExternal('https://github.com/losingenierosphp/AppMuni')
+              } 
+              },
+              {label:'Cerrar', 
+                click() { 
+                    app.quit() 
+                } }
+            ]
+        }
+    ])
+    Menu.setApplicationMenu(menu); 
+      //fin menu personalizado de la ventana electron 
 }
+
 
 app.on('ready', createWindow)
